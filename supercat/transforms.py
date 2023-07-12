d69a305046f9b1210971407c47977ef95c1719d5
rturnbull <robert.turnbull@unimelb.edu.au>
2023-07-13 08:43:53 +1000

    :zap: reading other file formats, not just the ones from the training dataset

--- supercat/transforms.py
+++ supercat/transforms.py
@@ -5,21 +5,38 @@ import numpy as np
 from pathlib import Path
 from fastai.vision.data import TensorImage
 from skimage.transform import resize as skresize
+from skimage import io
 
 
 DEEPROCK_HDF5_KEY = "temp"
 
 def read3D(path:Path):
-    data_dict = hdf5storage.loadmat(str(path))
-    if DEEPROCK_HDF5_KEY not in data_dict:
-        keys_found = ",".join(data_dict.keys())
-        raise Exception(f"expected key {DEEPROCK_HDF5_KEY} not found in '{path}'.\nCheck the following keys: {keys_found}")
+    path = Path(path)        
+    if path.suffix == ".mat":
+        data_dict = hdf5storage.loadmat(str(path))
+        if DEEPROCK_HDF5_KEY not in data_dict:
+            keys_found = ",".join(data_dict.keys())
+            raise Exception(f"expected key {DEEPROCK_HDF5_KEY} not found in '{path}'.\nCheck the following keys: {keys_found}")
 
-    return np.float32(data_dict[DEEPROCK_HDF5_KEY]/255.0)
+        result =  np.float32(data_dict[DEEPROCK_HDF5_KEY]/255.0)
+    else:
+        result = np.float32(io.imread(path))
+        
+    print("result.shape", result.shape)
+    print(result.min(), result.max())
+
+    result /= 255.0
+
+    return result
 
 
-def write3D(path, data):
-    hdf5storage.savemat(str(path), {DEEPROCK_HDF5_KEY:data*255.0}, format='7.3', oned_as='column', store_python_metadata=True)
+def write3D(path:Path, data):
+    path = Path(path)
+
+    if path.suffix == ".mat":
+        hdf5storage.savemat(str(path), {DEEPROCK_HDF5_KEY:data*255.0}, format='7.3', oned_as='column', store_python_metadata=True)
+    else:
+        io.imsave(path, data)
 
 
 def unsqueeze(inputs):
