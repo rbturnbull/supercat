ad10ee96b4a29c1b799a2dbaf562061bcae4e203
yuhsienchiang <yuhsien.chiang@student.unimelb.edu.au>
2023-08-23 19:26:06 +1000

    :zap: modify callback to provide model the correct input form

--- supercat/diffusion.py
+++ supercat/diffusion.py
@@ -127,36 +127,34 @@ class DDPMCallback(Callback):
         else:
             alpha_bar_t = self.alpha_bar[t, None, None, None, None]
         alpha_bar_t = alpha_bar_t.to(self.dls.device)
-        
+
         # noisify the image
         xt =  torch.sqrt(alpha_bar_t) * hr + torch.sqrt(1-alpha_bar_t) * noise 
-        
-        # Stack input with low-resolution image (upscaled) and noise level
-        self.learn.xb = (torch.cat([xt, lr, alpha_bar_t.repeat(1,1,*hr.shape[2:])], dim=1),)
+
+        # Stack input with low-resolution image (upscaled) at channel dim,
+        # then pass the stacked image along with the noise level as tuple to the model
+        self.learn.xb = (torch.cat([xt, lr], dim=1), alpha_bar_t)
         self.learn.yb = (noise,) # we are trying to predict the noise
 
 
 class DDPMSamplerCallback(DDPMCallback):
-    def before_batch(self):        
+    def before_batch(self):
         lr = self.xb[0]
-        
+
         # Generate a batch of random noise to start with
         xt = torch.randn_like(lr)
-        
-        outputs = [xt] 
+
+        outputs = [xt]
         for t in track(reversed(range(self.n_steps)), total=self.n_steps, description="Performing diffusion steps for batch:"):
             z = torch.randn(xt.shape, device=xt.device) if t > 0 else torch.zeros(xt.shape, device=xt.device)
             alpha_t = self.alpha[t] # get noise level at current timestep
             alpha_bar_t = self.alpha_bar[t]
             sigma_t = self.sigma[t]
-            model_input = torch.cat(
-                [xt, lr, alpha_bar_t.repeat(1,1,*lr.shape[2:]).to(xt.device)], 
-                dim=1,
-            )
-            predicted_noise = self.model(model_input)
-            
+
+            predicted_noise = self.model(torch.cat([xt, lr], dim=1), alpha_bar_t)
+
             # predict x_(t-1) in accordance to Algorithm 2 in paper
-            xt = 1/torch.sqrt(alpha_t) * (xt - (1-alpha_t)/torch.sqrt(1-alpha_bar_t) * predicted_noise)  + sigma_t*z 
+            xt = 1/torch.sqrt(alpha_t) * (xt - (1-alpha_t)/torch.sqrt(1-alpha_bar_t) * predicted_noise)  + sigma_t*z
             outputs.append(xt)
 
         # self.learn.pred = (torch.stack(outputs, dim=1),)
