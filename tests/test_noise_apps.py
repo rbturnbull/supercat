6f5fff167f98a3f40c99489770626a2ed6174e0d
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-09-01 11:55:00 +1000

    :cop: adding test for NoiseTensorGenerator #20

--- tests/test_noise_apps.py
+++ tests/test_noise_apps.py
@@ -1,8 +1,19 @@
 from torchapp.testing import TorchAppTestCase
-from supercat.noise.apps import NoiseSR
-
+from supercat.noise.apps import NoiseSR, NoiseTensorGenerator
+import torch
 
 class TestNoise(TorchAppTestCase):
     app_class = NoiseSR
 
 
+def test_noise_tensor_generator():
+    shape = (100, 100)
+    noise_generator = NoiseTensorGenerator(shape)
+
+    assert noise_generator.shape == shape
+    
+    output = noise_generator()
+    assert output.shape == (1,) + shape
+    assert isinstance(output, torch.Tensor)
+    assert output.min() < -0.99
+    assert output.max() > 0.99
