d7b1d0ffcebcb2d801860443507535bc1d963582
yuhsienchiang <yuhsien.chiang@student.unimelb.edu.au>
2023-10-23 17:20:10 +1100

    :bug:transform image to greyscale and resize to fixed shape

--- supercat/image/apps.py
+++ supercat/image/apps.py
@@ -4,6 +4,8 @@ import torchapp as ta
 from fastai.data.block import DataBlock
 from fastai.data.core import DataLoaders
 from fastai.data.transforms import RandomSplitter, get_image_files
+from fastai.vision.augment import Resize
+from fastai.vision.core import PILImageBW
 from fastai.vision.data import ImageBlock
 
 from supercat.noise.apps import NoiseSR
@@ -14,8 +16,11 @@ class ImageSR(NoiseSR):
         self,
         base_dir: Path = ta.Param(default=None, help="The base directory for the dataset."),
         dim: int = ta.Param(default=2, help="The dimension of the dataset. 2 or 3."),
+        depth: int = ta.Param(default=500, help="The depth of the noise image."),
+        width: int = ta.Param(default=500, help="The width of the noise image."),
+        height: int = ta.Param(default=500, help="The height of the noise image."),
         batch_size: int = ta.Param(default=16, help="The batch size for training."),
-        valid_proportion: float = ta.Param(defalut=0.2, help="The proportion of the dataset to use for validation."),
+        valid_proportion: float = ta.Param(default=0.2, help="The proportion of the dataset to use for validation."),
         split_seed: int = 42,
     ):
         assert base_dir is not None, "You must specify a base directory for the dataset."
@@ -25,10 +30,13 @@ class ImageSR(NoiseSR):
         images = get_image_files(base_dir)
 
         self.dim = dim
+        height = height or width
+        depth = depth or width
 
         datablock = DataBlock(
-            blocks=(ImageBlock),
+            blocks=(ImageBlock(cls=PILImageBW)),
             splitter=RandomSplitter(valid_proportion, seed=split_seed),
+            item_tfms=Resize((height, width)) if dim == 2 else Resize((depth, height, width)),
         )
 
         dataloaders = DataLoaders.from_dblock(
