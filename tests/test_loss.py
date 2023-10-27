1c886e88ae6704c2fac260208e321f72adadfbf1
Charlie Wang <tingxuanw1@4180L-213535-M.local>
2023-10-27 14:31:54 +1100

    :cop: adding test for edge loss in 3d'

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -19,3 +19,20 @@ def test_edge_loss2d():
     assert loss < 0.001
 
     assert 0.0034 < loss_module( target+0.1, target ) < F.smooth_l1_loss(target+0.1, target)
+
+
+def test_edge_loss3d():
+    seed = 42
+    density = 23
+    shape = (32, 32, 32)
+    wn = WorleyNoiseTensor(shape, density, seed=seed)
+    n_samples = 2
+
+    target = torch.zeros( (n_samples,) + shape )
+    for i in range(n_samples):
+        target[i] = wn()
+
+    loss_module = EdgeLoss(percentile=50, alpha=0.5)
+    loss = loss_module( target, target )
+    assert loss < 0.001
+    assert 0.0036 < loss_module( target+0.1, target ) < F.smooth_l1_loss(target+0.1, target)
